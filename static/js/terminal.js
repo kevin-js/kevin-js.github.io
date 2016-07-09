068e78e0dc0344a5325b375460e5967f93489a43
Kevin Guh <kevinjguh@yahoo.com>
2016-07-08 20:20:51 -0700

    added youtube link

---
+++ static/js/terminal.js
@@ -0,0 +1,20 @@
+var Terminal = React.createClass({
+	render: function(){
+		return (
+			<div>
+			</div>
+		);
+	}
+});
+
+var Prompt = React.createClass({
+	render: function(){
+		<div className="">
+		</div>
+	}
+});
+
+ReactDOM.render(
+	<Terminal />,
+	document.getElementById('terminal')
+);
